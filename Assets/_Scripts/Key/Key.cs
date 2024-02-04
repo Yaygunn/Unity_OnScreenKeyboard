25c342c412a585a4b2f0366474f78e3bdc79eaf7
Yaygun <yusufaygunnn@hotmail.com>
2024-02-04 22:51:15 +0300

    Key prefab
    A key prefab made to so spawning keys from keyboard controller can be implemented

---
+++ Assets/_Scripts/Key/Key.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] private char key;
+    [SerializeField] private TextMeshProUGUI textMeshPro;
+
+    public void ButtonClicked()
+    {
+        print(key);//change this
+    }
+    private void Start()
+    {
+        ShowKey();
+    }
+    public void SetKey(char key)
+    {
+        this.key = key;
+        ShowKey();
+    }
+
+    private void ShowKey()
+    {
+        textMeshPro.text=key.ToString();
+    }
+}
