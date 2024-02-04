b634ff0471b550d57f9deb9868bd3fc606345603
Yaygun <yusufaygunnn@hotmail.com>
2024-02-04 22:39:40 +0300

    Keyboard Transform
    Transform Settings for keyboard can be changed from editor

--- Assets/_Scripts/Keyboard/KeyboardController.cs
+++ Assets/_Scripts/Keyboard/KeyboardController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class KeyboardController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private KeyboardModel model;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        SetKeyboardTransform();
     }
+
+    private void SetKeyboardTransform()
+    {
+        // keyboard size
+        float width = Screen.width * model.widthPercentage;
+        float height = Screen.height * model.heightPercentage;
+
+        model.rectTransform.sizeDelta = new Vector2(width, height);
+
+        // keyboard position
+        Vector2 pos = new Vector2(0, 0);
+
+        pos.x = Screen.width / 2;
+        pos.y = model.bottomOffset * Screen.height + height / 2;
+
+        model.rectTransform.position = pos;
+
+    }
+
 }
--- Assets/_Scripts/Keyboard/KeyboardModel.cs
+++ Assets/_Scripts/Keyboard/KeyboardModel.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class KeyboardModel : MonoBehaviour
 {
-    
+    [SerializeField] public RectTransform rectTransform;
+
+    [Header("Size")]
+    [Range(0f, 1f)]
+    [SerializeField] public float widthPercentage;
+    [Range(0f, 1f)]
+    [SerializeField] public float heightPercentage;
+    [Range(0f, 0.5f)]
+    [SerializeField] public float bottomOffset;
+
 }
